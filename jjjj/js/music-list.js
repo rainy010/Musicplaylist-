54b41ed26d52f2e45b5225f9028373b8a7edb8af
rainy010 <151761847+rainy010@users.noreply.github.com>
2023-11-28 09:37:11 -0600

    Add files via upload

---
+++ jjjj/js/music-list.js
@@ -0,0 +1,45 @@
+let allMusic = [
+
+    {
+        name: "Yume Wo Kanaete Doraemon (instrumental)",
+        artist: "Doraemon",
+        img: "doraemon",
+        src: "music-1"
+    },
+    {
+        name: "Let It Go",
+        artist: "James Bay",
+        img: "music-2",
+        src: "spotifydown.com - Let It Go"
+    },
+    {
+        name: "Belong To You《我凭本事单身》Professional Single OST ENG-INDO",
+        artist: "Lirikis",
+        img: "maxresdefault",
+        src: "Belong To You - Sunnee Lyrics  《我凭本事单身》Professional Single OST  ENG-INDO SUB By Lirikis.mp4"
+    },
+    {
+        name: "Nevermind",
+        artist: "Babygirl",
+        img: "music-4",
+        src: "Nevermind"
+    },
+    {
+        name: "You Are The Reason",
+        artist: "Calum Scott",
+        img: "music-5",
+        src: "You Are The Reason"
+    },
+    {
+        name: "Last Christmas",
+        artist: "Wham!",
+        img: "music-6",
+        src: "Last Christmas"
+    },
+    {
+        name: "All I Want for Christmas Is You",
+        artist: "Mariah Carey",
+        img: "music-7",
+        src: "All I Want for Christmas Is You"
+    },
+    ];
